1aa77b6c6f64bfd13fcb45bec68e19d1fdca59d4
Prakash Chandra <prakashworks98@gmail.com>
2026-06-29 15:24:11 +0545

    used the ListView method to show the contents on the screen

--- message-board/posts/views.py
+++ message-board/posts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
+# NORMAL METHOD
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, "post_list.html", {"posts": posts})
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "post_list.html", {"posts": posts})
+# LIST VIEW METHOD
+
+
+class PostList(ListView):
+    model = Post
+    template_name = "post_list.html"
 
 
 # Create your views here.
