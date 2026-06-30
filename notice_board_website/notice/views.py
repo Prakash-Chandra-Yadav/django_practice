8984690e1c470e757f2b8cc7e48fec0a23effa05
Prakash Chandra <prakashworks98@gmail.com>
2026-06-30 13:03:42 +0545

    added the data on the homepage by using the get_context() method

--- notice_board_website/notice/views.py
+++ notice_board_website/notice/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomepageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["company_name"] = "ABC pvt.ltd"
+        context["company_type"] = "finance"
+        return context
