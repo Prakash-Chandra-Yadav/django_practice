c6e8eacdc593997a9f6188552244c7bc80cd3d4b
Prakash Chandra <prakashworks98@gmail.com>
2026-06-30 12:26:18 +0545

    configured the app in the project settings

--- notice_board_website/django_project/settings.py
+++ notice_board_website/django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "notice",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "Templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
